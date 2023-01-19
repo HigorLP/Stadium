155d784bce5b78dbb6059a0a6d80266356c8037c
Higor Pereira <higor.leandro@eb.mil.br>
2023-01-18 22:21:57 -0300

    First CProject Creation and Start of the Ticket Class.ommit

---
+++ Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stadium {
+    internal class Ticket {
+        private string price;
+
+        public Ticket(string price) {
+            this.Price = price;
+        }
+
+        public string Price { get => price; set => price = value; }
+
+
+        public void showPrice() {
+            Console.Write(Price);
+        }
+    }
+}
